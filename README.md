a7d4efb59211d8cc690949e84681ce260d9e3c24
Colby Fayock <fayock@gmail.com>
2020-02-08 12:57:57 -0500

    script and readme

--- README.md
+++ README.md
@@ -1,6 +1,60 @@
-# slack-emoji-transfer
+# Slack Emoji Transfer
 
-WIP
+A node script that makes it ~easy~ easier to import all emojis from one Slack workspcae into another.
 
-* yarn install
-* node index.js
+## Before You Use This
+*These actions are undoable* - once you successfully run this script, it will have added emoji to the Slack you're transferring to.
+
+## Getting Started
+### Prerequisities
+* Oauth token from the Slack you're transferring *from* (`xoxp-*`)
+* User token from the Slack you're transferring *to* (`xoxs-*`)
+**See below for how to grab tokens.**
+
+### How to Use
+* Obtain tokens for the Slack you're transfering from and the Slack you're trasnfering to
+* Run `yarn install` to pull in dependencies
+* Run `yarn transfer` to initiate the script
+* Follow the prompts to enter your token, optionally preview, and to run the scripts
+
+## How this works?
+### Slack API
+There are 2 methods of the Slack API being utilize to make this work, `emoji.list` and `emoji.add`. `emoji.list` [is documented](https://api.slack.com/methods/emoji.list) however `emoji.list` [is not](https://webapps.stackexchange.com/a/126154).
+
+* `emoji.list`: used along side the Oauth token specified in the from prompt to grab all of the emojis from the workspace
+* `emoji.add`: used along side the User token specified in the to prompt to add each individual emoji
+
+### Rate Limits
+To avoid too many requests, the script breaks up the requests in batches and performs the requests in the frequency at the top of the `index.js` file.
+
+Rate limit information can be found here: https://api.slack.com/docs/rate-limits
+
+## How to Grab Tokens
+
+If you're doing this as a 1 off (which is what I did), you can use the following methods to grab your tokens. If you're more familiar or trying to integrate something properly with the Slack API, you probably want to follow their instructions for API integration.
+
+### Oauth Token
+> Requirement: the token must match the pattern `xoxp-*`
+
+To obtain an Oauth token for a 1 off request, you can utilize Slack's Legacy Token tool to generate a token.
+
+* Log in to your Slack workspace that you want to import from
+* Visit https://api.slack.com/legacy/custom-integrations/legacy-tokens
+* Next to the workspace you would like to generate, click Create Token or if you already have one generated, simply copy and paste the available token for future use
+
+### User Token
+> Requirement: the token must match the pattern `xoxs-*`
+
+Unfortunately as I'm writing this, there's not an easy way to obtain this token. Instead what we can do is log in to our Slack workspace and grab the User token your active session is using to make the requests in the web app.
+
+* Log in to your Slack workspace that you want to transfer to
+* Visit the Customize section of the web app (or https://my.slack.com/customize)
+* Open up your browser's developer tools, particularly the web console that will allow you to run commands ([Chrome](https://developers.google.com/web/tools/chrome-devtools), [Firefox](https://developer.mozilla.org/en-US/docs/Tools))
+* Run the following ocmmand `window.prompt('API Token', TS.boot_data.api_token)`
+* Copy the value for future use
+
+**Thanks @jackellenberger via https://github.com/jackellenberger/emojme#finding-a-slack-token**
+
+## TODO
+Feel free to submit a PR if you're feeling ambitious :)
+* Alias support: currently it just errors when trying to add
--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 const prompt = require('prompt');
 const axios = require('axios');
+const FormData = require('form-data');
 
 prompt.message = 'Slack Transfer';
 prompt.delimiter = ' > ';
 
+const REQUESTS_PER_BATCH = 20;
+const SECONDS_PER_BATCH = 60;
+
 const SLACK_API_EMOJI_LIST = 'https://slack.com/api/emoji.list';
+const SLACK_API_EMOJI_ADD = 'https://slack.com/api/emoji.add';
 
 (async () => {
   prompt.start();
 
-  const { keyFrom, keyTo } = await promiseToGetPrompt([
+  const { tokenFrom, tokenTo } = await getPrompt([
     {
       properties: {
-        keyFrom: {
-          description: 'What API do you want to use to transfer from?',
+        tokenFrom: {
+          description: 'Slack From (Oauth Token)',
           required: true
         }
       }
     },
     {
       properties: {
-        keyTo: {
-          description: 'What API do you want to use to transfer to?',
+        tokenTo: {
+          description: 'Slack To (User Token)',
+          pattern: /^xoxs\-/,
+          message: 'Must start with "xoxs-"',
           required: true
         }
       }
     }
   ]);
 
-  const { data: listData = {} } = await axios.get(`${SLACK_API_EMOJI_LIST}?token=${keyFrom}`);
+  const { data: listData = {} } = await axios.get(`${SLACK_API_EMOJI_LIST}?token=${tokenFrom}`);
   const { emoji } = listData;
   const emojiKeys = Object.keys(emoji);
 
-  const { shouldPreview } = await promiseToGetPrompt([
+  const emojisToUpload = emojiKeys.map(key => {
+    return {
+      name: key,
+      url: emoji[key]
+    }
+  });
+
+  const batches = batchItems(emojisToUpload, REQUESTS_PER_BATCH);
+  const batchesLength = batches.length;
+
+  console.log(`Batches to process: ${batchesLength}`);
+  console.log(`Items per batch: ${REQUESTS_PER_BATCH}`);
+  console.log(`Seconds per batch: ${SECONDS_PER_BATCH}`);
+
+  const { shouldPreview } = await getPrompt([
     {
       properties: {
         shouldPreview: {
@@ -46,7 +67,7 @@ const SLACK_API_EMOJI_LIST = 'https://slack.com/api/emoji.list';
   if ( shouldPreview === 'y' ) {
     emojiKeys.forEach(key => console.log(key));
 
-    const { shouldContinue } = await promiseToGetPrompt([
+    const { shouldContinue } = await getPrompt([
       {
         properties: {
           shouldContinue: {
@@ -62,19 +83,168 @@ const SLACK_API_EMOJI_LIST = 'https://slack.com/api/emoji.list';
     }
   }
 
+  await Promise.all(batches.map((batch, index) => {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        const all = await addNewEmojis(batch, tokenTo);
+        const successes = all.filter(response => !!response.success);
+        const failures = all.filter(response => !response.success);
+
+        console.log('Successfully added...');
+
+        successes.forEach(({ name, url}) => console.log(` > ${name} - ${url}`));
+
+        console.log('Failed to be added...');
+
+        failures.forEach(({ name, url, error}) => console.log(` > ${name} - ${url} - ${error.message}`));
+
+        console.log(`Batch ${index + 1} out of ${batchesLength}`)
+
+        if ( index + 1 < batchesLength ) {
+          console.log(`Waiting to process next batch (${SECONDS_PER_BATCH}s/batch)...`);
+        }
+
+        resolve(all);
+      }, SECONDS_PER_BATCH * 1000 * index);
+    })
+  }))
+
+  console.log('Done.');
+
 })();
 
-function promiseToGetPrompt(list) {
+/**
+ * addNewEmojis
+ * @description Adds a list of emojis to a slack workspace
+ */
+
+async function addNewEmojis(emojis, token) {
+  const promises = emojis.map(async emoji => {
+    let response;
+    try {
+      response = await postNewEmoji({
+        ...emoji,
+        token
+      });
+      if ( !response.ok ) throw new Error(response.error);
+      return {
+        ...emoji,
+        success: true
+      }
+    } catch(e) {
+      return {
+        ...emoji,
+        success: false,
+        error: e
+      };
+    }
+  })
+
+  return await Promise.all(promises);
+}
+
+/**
+ * postNewEmoji
+ * @description POST a new emoji to Slack
+ */
+
+async function postNewEmoji({ name, url, token }) {
+  const errorBase = 'Failed to post new emoji';
+
+  const formData = new FormData();
+  let imageBuffer;
+
+  formData.append('name', name);
+  formData.append('mode', 'data');
+
+  try {
+    imageBuffer = await getImageBuffer(url);
+  } catch(e) {
+    throw new Error(`${errorBase}: ${e}`);
+  }
+
+  formData.append('image', imageBuffer, name);
+
+  try {
+    const { data } = await axios({
+      method: 'post',
+      url: SLACK_API_EMOJI_ADD,
+      data: formData.getBuffer(),
+      headers: {
+        ...formData.getHeaders(),
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    return data;
+  } catch(e) {
+    throw new Error(`${errorBase}: ${e}`)
+  }
+}
+
+/**
+ * getImageBuffer
+ * @description GET an image and return it's Buffer
+ */
+
+async function getImageBuffer(url) {
+  const errorBase = 'Failed to get image';
+
+  if ( typeof url !== 'string' ) {
+    throw new Error(`${errorBase}: Invalid url - ${url}`);
+  }
+
+  try {
+    const { data: imageData } = await axios.get(url, {
+      responseType: 'arraybuffer'
+    });
+    return imageData;
+  } catch(e) {
+    throw new Error(`${errorBase}: ${e}`);
+  }
+}
+
+/**
+ * getPrompt
+ * @description Promise to get user input given the prompt options
+ */
+
+function getPrompt(options) {
   const errorBase = 'Failed to get prompt';
   return new Promise((resolve, reject) => {
-    if ( !Array.isArray(list) ) {
-      reject(`${errorBase}: Invalid list`);
+    if ( !Array.isArray(options) ) {
+      reject(`${errorBase}: Invalid options`);
     }
-    prompt.get(list, function (err, result) {
+    prompt.get(options, function (err, result) {
       if ( err ) {
         reject(`${errorBase}: ${err}`);
       }
       resolve(result);
     });
   });
+}
+
+/**
+ * batchItems
+ * @description Splits an array into a group of arrays with a specificied number of items
+ */
+
+function batchItems(items, itemsPerBatch) {
+  const errorBase = 'Failed to batch items';
+
+  if ( !Array.isArray(items) ) {
+    throw new Error(`${errorBase}: invalid items ${items}`);
+  }
+
+  const batchCount = Math.ceil(items.length / itemsPerBatch);
+  const batches = [];
+
+  for ( let i = 0; i < batchCount; i++ ) {
+    const start = i * itemsPerBatch;
+    const end = start + itemsPerBatch;
+    const batch = items.slice(start, end)
+    batches.push(batch);
+  }
+
+  return batches;
 }
